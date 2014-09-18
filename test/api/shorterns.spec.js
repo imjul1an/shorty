804562b4866f3010620dfd08c9f5dcb8c83bb69f
juliausti <julian.ustiyanovych@gmail.com>
2014-09-18 17:14:22 +0300

    Add test for request POST api/shorten with required field: url

--- test/api/shorterns.spec.js
+++ test/api/shorterns.spec.js
@@ -8,7 +8,7 @@ describe('shorterns.spec.js', function () {
 		apiUrl = testUtils.getRootUrl() + '/api/shorten';
 	});
 
-	describe('when create request to short the link', function () {
+	describe('when create request to short the url', function () {
 		beforeEach(function(){
 			url = apiUrl;
 		});
@@ -17,20 +17,44 @@ describe('shorterns.spec.js', function () {
 			beforeEach(function (){
 				payload = {};
 			});
-		});
 
-		beforeEach(function (done){
-			request.post({url: url, body: payload, json:true}, function (err, res, body) {
-				response = res;
-				body = body;
-				error = err;
-				done(err);
+			beforeEach(function (done){
+				request.post({url: url, body: payload, json:true}, function (err, res, body) {
+					response = res;
+					body = body;
+					error = err;
+					done(err);
+				});
+			});
+
+			it('should respond 400 (Bad Request)', function () {
+				expect(response.statusCode).to.equal(400);
 			});
 		});
+	});
 
-		it('should respond 400 (Bad Request)', function () {
-			expect(response.statusCode).to.equal(400);
+	describe('when create request to short the url', function () {
+		beforeEach(function(){
+			url = apiUrl;
 		});
 
+		describe('and url is present', function() {
+			beforeEach(function (){
+				payload = {url: 'http://example.com/very-long-url'};
+			});
+
+			beforeEach(function (done){
+				request.post({url: url, body: payload, json:true}, function (err, res, body) {
+					response = res;
+					body = body;
+					error = err;
+					done(err);
+				});
+			});
+
+			it('should respond 201 (Created)', function () {
+				expect(response.statusCode).to.equal(201);
+			});
+		});
 	});
 });
