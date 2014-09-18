6b7570da312a307a8da3f93111aa635c3d8a3944
juliausti <julian.ustiyanovych@gmail.com>
2014-09-18 12:39:46 +0300

    Add configs

---
+++ config/test.config.js
@@ -0,0 +1,11 @@
+var config = {
+	connection: 'mongodb://localhost:27017/shorty',
+	options: { auto_reconnect: true },
+
+	applicationUrl: 'http://localhost:5000',
+	logentries: {
+		token: '32a06cd0б-f99c-4625-a538-3afd98016248'
+	}
+};
+
+module.exports = config;
